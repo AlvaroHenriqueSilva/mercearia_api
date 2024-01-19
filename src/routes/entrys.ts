c687e1c15489a26bb27b33710776159920a9066b
ALVARO HENRIQUE GOMES DA SILVA <alvaro_silva@estudante.sc.senai.br>
2024-01-19 14:35:40 -0300

    feat: adicionando o controller a rota de entrada_estoque

--- src/routes/entrys.ts
+++ src/routes/entrys.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import CreateEntryController from "../controllers/entrys/CreateEntryController";
 
 const entrys = Router()
 
-entrys.post('/products/entry/:id')
+entrys.post('/products/entry/:id', CreateEntryController)
 
 export default entrys
